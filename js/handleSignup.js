adcb1818d0cd9d0907e34da177853f113c9e4722
Salami Azeez <salamitopeazeez@gmail.com>
2024-10-26 21:49:16 +0100

    Show multi-form page on Submit for signup

---
+++ js/handleSignup.js
@@ -0,0 +1,11 @@
+
+const regForm = document.getElementById("regForm")
+
+regForm.addEventListener("submit", (e) => {
+    e.preventDefault()
+ 
+    window.location = "./multi.html";
+    window.location.href = "./multi.html";
+    window.location.replace("./multi.html");
+
+})
--- js/index.js
+++ js/index.js
@@ -2,11 +2,39 @@ const form1 = document.getElementById('form1')
 const form2 = document.getElementById('form2')
 const form3 = document.getElementById('form3')
 const form4 = document.getElementById('form4')
+const eyeIcon = document.getElementById('eyeIcon')
 
 const progressBar = document.getElementById("progress")
 const circles = document.querySelectorAll(".circle")
 
 let currentActive = 1;
+
+// =================== Signup page submission
+const regForm = document.getElementById("regForm")
+
+regForm?.addEventListener("submit", (e) => {
+    e.preventDefault()
+
+    const successReg = './multi.html'
+    window.open(successReg)
+
+
+})
+
+// ======================= reveal password
+function showPassword() {
+    const passwordInput = document.getElementById('password');
+    const toggleButton = document.getElementById('togglePassword');
+    const eyeIcon = document.getElementById('eyeIcon');
+    const inputType = passwordInput.type === 'password' ? 'text' : 'password';
+    passwordInput.type = inputType;
+
+    // Change the icon based on visibility
+    eyeIcon.textContent = inputType === 'password' ? '👁️' : '🙈'; // Open eye icon
+}
+
+
+
 
 
 // ==================== Progress Bar update
@@ -14,35 +42,35 @@ const update = () => {
     currentActive++
     console.log(currentActive)
     console.log(circles.length)
-    if(currentActive > circles.length) {
+    if (currentActive > circles.length) {
         currentActive = circles.length
     }
     circles.forEach((circle, i) => {
-        if(i < currentActive) {
+        if (i < currentActive) {
             circle.classList.add('active')
-        }else {
+        } else {
             circle.classList.remove('active')
         }
 
         const active = document.querySelectorAll('.active')
-        progressBar.style.width = ((active.length-1) / (circles.length-1)) * 100 + '%'
+        progressBar.style.width = ((active.length - 1) / (circles.length - 1)) * 100 + '%'
     })
 }
 // =================== Go to next form
-function nextItemOne () {
+function nextItemOne() {
     form1.style.display = 'none'
     console.log("First next clicked")
     form2.style.left = '64px'
 
     update()
 }
-function nextItemTwo () {
+function nextItemTwo() {
     form2.style.display = 'none'
     form3.style.left = '64px'
 
     update()
 }
-function nextItemThree () {
+function nextItemThree() {
     console.log("First next clicked")
     form3.style.display = 'none'
     form4.style.left = '64px'
@@ -56,9 +84,9 @@ const btnsEvent = () => {
     const next3 = document.getElementById("next3")
 
     //===========
-    next1.addEventListener('click', nextItemOne)
-    next2.addEventListener('click', nextItemTwo)
-    next3.addEventListener('click', nextItemThree)
+    next1?.addEventListener('click', nextItemOne)
+    next2?.addEventListener('click', nextItemTwo)
+    next3?.addEventListener('click', nextItemThree)
 }
 
 document.addEventListener('DOMContentLoaded', btnsEvent)
